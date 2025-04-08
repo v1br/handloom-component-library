2b2f3cbef432f26e291541438cbd6cbb66a79ce2
v1br <vibhorag2003@gmail.com>
2025-04-08 21:09:18 +0530

    chore: format config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		"./src/**/*.{ts,tsx}",
-	],
+	content: ["./src/**/*.{ts,tsx}"],
 	theme: {
-		extend: {}
+		extend: {},
 	},
-	plugins: []
-}
+	plugins: [],
+};
